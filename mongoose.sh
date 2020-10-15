b60e967a7b009d871585cb03eaab5c65bc190a4d
Salvador Fco Criado Melero <36660968+salvacnj@users.noreply.github.com>
2020-10-15 12:36:21 +0200

    Update mongoose.sh

--- mongoose.sh
+++ mongoose.sh
@@ -8,18 +8,12 @@ echo "#####################################
 
 npm install mongoose 
 
-
 echo "
 
 # MONGO ENVIROMENTS
+MONGO_URL='mongodb+srv://nazaries:mongodb_nazaries@cluster0-yidbh.mongodb.net/iot_farm?retryWrites=true&w=majority'
 
-MONGO_HOST=localhost
-MONGO_PORT=27017
-MONGO_DB=
-MONGO_USER=
-MONGO_PASS=
 " >> ./.env 
-
 
 echo "
 /*********************************/
@@ -28,7 +22,7 @@ echo "
 
 const mongoose = require('mongoose');
 
-var mongo_uri = process.env.MONGO_URL || `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+var mongo_uri = process.env.MONGO_URL;
 //var mongo_uri = 'mongodb://'process.env.MONGO_USER + ':' +process.env.MONGO_PASS + '@' + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_DB;
 
 mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, res) {
